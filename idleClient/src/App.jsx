0c08b18ff1e50b04e4ea757241124c398a68350a
Jaedyn Keister <Jaedyn.keister@gmail.com>
2022-12-28 14:06:09 -0600

    Test Pushing to my Branch

--- idleClient/src/App.jsx
+++ idleClient/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+  const [active, setActive] = useState('')
+
+  function someFunction(){
+    
+  }
+
+  useEffect(()=>{
+    someFunction()
+  },[active])
+
   return (
     <div className="App">
+      <button onClick={setActive('test')}></button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
