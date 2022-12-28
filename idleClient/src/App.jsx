4a7312a8e1ad6192a837026fd60adaf4fcfd995d
jatanolu <jacobrjean@gmail.com>
2022-12-28 17:54:47 -0500

    added blank routes and navbar

--- idleClient/src/App.jsx
+++ idleClient/src/App.jsx
@@ -1,37 +1,58 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'
+import SignInComp from '../components/signin.jsx'
+import Home from '../components/home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
 
   return (
-    <div className="App">
-      <h1>Jaedyn</h1>
-      <div>
-        <h1>Bryan</h1>
-        <h1>Nick K</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+	  <>
+
+    <Navbar bg="light" expand="lg" fixed='top'>
+      <Container>
+        <Navbar.Brand href="/">IDLE TIME</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+	  <div className='d-flex position-absolute start-50'>
+            <Nav.Link className='test' href="/#/SignIn">Link</Nav.Link>
+            <NavDropdown className='test1' title="Profile" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+	    </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+	<Router>
+		<Routes>
+			<Route path='/' element={<Home />}></Route>
+			<Route path='/SignIn' element={<SignInComp />}></Route>
+			<Route></Route>
+		</Routes>
+	</Router>
+	  </>
   )
 }
 
