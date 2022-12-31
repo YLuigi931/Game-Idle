eb0411fa05287f2a131accd924d41d31845d8a69
jatanolu <jacobrjean@gmail.com>
2022-12-30 19:10:35 -0500

    added a background and signin form

--- idleClient/components/signin.jsx
+++ idleClient/components/signin.jsx
@@ -1,6 +1,25 @@
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+
 function SignInComp(){
 	return (
 	<>
+	<div className='box position-absolute top-50 start-50 translate-middle'>
+	<Form>
+      <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+
+	<div className='input-group'>
+      <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+	<Button className='input-group-append'>Submit</Button>
+	</div>
+	</Form>	
+	</div>
+
 	</>
 	)
 }
