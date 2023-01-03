e720db8d5b396ae485ab2477231d114ea0af2a04
jatanolu <jacobrjean@gmail.com>
2023-01-03 10:14:25 -0500

    combine current work

--- idleServer/idleServerApp/models.py
+++ idleServer/idleServerApp/models.py
@@ -31,3 +31,29 @@ class AppUser(AbstractUser):
 # class Enemy(models.Model):
 #     name = models.CharField(max_length=15)
    
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    max_stacks = models.PositiveIntegerField()
+    rarity = models.CharField(max_length=10)
+    description = models.TextField(max_length=255)
+
+
+class Weapon(Item):
+    attack = models.PositiveIntegerField()
+
+class Armor(Item):
+    defense = models.PositiveIntegerField()
+
+class Character(models.Model):
+    name = models.CharField(max_length=15)
+    user_character = models.ForeignKey(AppUser, on_delete=models.DO_NOTHING, related_name='user_character')
+
+class Enemy(models.Model):
+    name = models.CharField(max_length=15)
+
+
+class Inventory(models.Model):
+    user = models.ForeignKey(Character, on_delete=models.DO_NOTHING, related_name='character_inventory')
+    max_spaces = models.PositiveIntegerField()
+    in_inventory = ArrayField(models.JSONField(null=True, blank=True), blank=True)
