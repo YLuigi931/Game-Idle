11d21fc2e18cf99452dd4c2a5612032219a79906
Scylith <scylith@gmail.com>
2023-01-04 13:33:48 -0600

    add/delete item working

--- idleServer/idleServerApp/models.py
+++ idleServer/idleServerApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import (AbstractUser)
+from django.contrib.postgres.fields import ArrayField
+
 
 class AppUser(AbstractUser):
     """
@@ -22,14 +23,23 @@ class AppUser(AbstractUser):
        ''',
     )
     REQUIRED_FIELDS = []
-# Create your models here.
+
+
+
+# class Character(models.Model):
+#     name = models.CharField(max_length=15)
+#     user_character = models.ForeignKey(AppUser, on_delete=models.DO_NOTHING, related_name='user_character')
+
+# class Enemy(models.Model):
+#     name = models.CharField(max_length=15)
+   
 class Item(models.Model):
     name = models.CharField(max_length=255)
     quantity = models.PositiveIntegerField()
     max_stacks = models.PositiveIntegerField()
     rarity = models.CharField(max_length=10)
     description = models.TextField(max_length=255)
-    
+
 
 class Weapon(Item):
     attack = models.PositiveIntegerField()
@@ -37,15 +47,17 @@ class Weapon(Item):
 class Armor(Item):
     defense = models.PositiveIntegerField()
 
-class Character(models.Model):
-    name = models.CharField(max_length=15)
-    user_character = models.ForeignKey(AppUser, on_delete=models.DO_NOTHING, related_name='user_character')
-
 class Enemy(models.Model):
     name = models.CharField(max_length=15)
 
 
 class Inventory(models.Model):
-    user = models.ForeignKey(Character, on_delete=models.DO_NOTHING, related_name='character_inventory')
     max_spaces = models.PositiveIntegerField()
-    in_inventory = ArrayField(models.JSONField(null=True, blank=True), blank=True)
+    weapon_inventory = ArrayField(models.CharField(max_length=200),blank=True, null=True)
+    armor_inventory = ArrayField(models.CharField(max_length=200),blank=True, null=True)
+    item_inventory = ArrayField(models.CharField(max_length=200),blank=True, null=True)
+
+class Character(models.Model):
+    name = models.CharField(max_length=15)
+    user_character = models.ForeignKey(AppUser, on_delete=models.DO_NOTHING, related_name='user_character')
+    userInventory = models.ForeignKey(Inventory, on_delete=models.DO_NOTHING, related_name='character_inventory', blank=True, null=True)
